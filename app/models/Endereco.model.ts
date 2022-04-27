09d4b2af52874d5a250eae7795b6ec282ee111e8
Luan Oliveira <luan.oliveira@pebmed.com.br>
2022-04-27 18:45:03 -0300

    Adicionando models do banco de dados

---
+++ app/models/Endereco.model.ts
@@ -0,0 +1,44 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { Usuario } from './Usuario.model';
+
+@Entity()
+export class Endereco {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'text', nullable: false })
+    logradouro: string;
+
+    @Column({ length: 8, nullable: false })
+    numero: string;
+
+    @Column({ length: 64, nullable: false })
+    bairro: string;
+
+    @Column({ length: 64, nullable: false })
+    cidade: string;
+
+    @Column({ length: 64, nullable: false })
+    estado: string;
+
+    @Column({ length: 32, nullable: false })
+    pais: string;
+
+    @Column({ length: 8, nullable: true })
+    cep: string;
+
+    @Column({ default: () => 'CURRENT_TIMESTAMP' })
+    criadoEm: Date;
+
+    @Column({ nullable: true })
+    atualizadoEm: Date;
+
+    @Column({ nullable: false })
+    usuarioId: number;
+
+    @ManyToOne(() => Usuario)
+    @JoinColumn({ name: 'usuarioId' })
+    usuario: Usuario;
+
+}
---
+++ app/models/Usuario.model.ts
@@ -0,0 +1,37 @@
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Endereco } from './Endereco.model';
+
+@Entity()
+export class Usuario {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ nullable: false })
+    nome: string;
+
+    @Column({ length: 320, unique: true, nullable: false })
+    email: string;
+
+    @Column({ length: 72, nullable: false })
+    senha: string;
+
+    @Column({ length: 11, unique: true, nullable: true })
+    cpf: string;
+
+    @Column({ length: 11, nullable: true })
+    telefone: string;
+
+    @Column({ nullable: false })
+    comunicacoes: boolean;
+
+    @Column({ default: () => 'CURRENT_TIMESTAMP' })
+    criadoEm: Date;
+
+    @Column({ nullable: true })
+    atualizadoEm: Date;
+
+    @OneToMany(() => Endereco, endereco => endereco.usuario, { cascade: true })
+    enderecos: Endereco[];
+
+}
