a968e5d5326dd168756d450d7f3c3cc4e67b7e89
Luan Oliveira <luan.oliveira@pebmed.com.br>
2022-05-02 08:37:15 -0300

    Adicionando injeção de dependência

--- app/dependencies/controllers.ts
+++ app/dependencies/controllers.ts
@@ -1,7 +1,13 @@
 import { container } from 'tsyringe';
 import { HealthcheckController } from '../controllers/Healthcheck/Healthcheck.controller';
+import { CadastrarUsuarioController } from '../controllers/Usuario/Cadastrar/CadastrarUsuario.controller';
 
 container.registerSingleton<HealthcheckController>(
     'HealthcheckController',
     HealthcheckController,
+);
+
+container.registerSingleton<CadastrarUsuarioController>(
+    'CadastrarUsuarioController',
+    CadastrarUsuarioController,
 );
--- app/dependencies/usecases.ts
+++ app/dependencies/usecases.ts
@@ -1,7 +1,13 @@
 import { container } from 'tsyringe';
 import { HealthcheckUsecase } from '../usecases/Healthcheck/Healthcheck.usecase';
+import { CadastrarUsuarioUsecase } from '../usecases/Usuario/CadastrarUsuario.usecase';
 
 container.registerSingleton<HealthcheckUsecase>(
     'HealthcheckUsecase',
     HealthcheckUsecase,
+);
+
+container.registerSingleton<CadastrarUsuarioUsecase>(
+    'CadastrarUsuarioUsecase',
+    CadastrarUsuarioUsecase,
 );
