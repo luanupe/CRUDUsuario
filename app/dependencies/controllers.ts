68720ecb0d6ee6e565f10c1d8e8973d3d070c314
Luan Oliveira <luan.oliveira@pebmed.com.br>
2022-05-04 13:31:37 -0300

    Adiciona injeções de dependência

--- app/dependencies/controllers.ts
+++ app/dependencies/controllers.ts
@@ -1,13 +1,29 @@
 import { container } from 'tsyringe';
 import { HealthcheckController } from '../controllers/Healthcheck/Healthcheck.controller';
+import { AutenticarUsuarioController } from '../controllers/Usuario/Autenticar/AutenticarUsuario.controller';
 import { CadastrarUsuarioController } from '../controllers/Usuario/Cadastrar/CadastrarUsuario.controller';
+import { VisualizarUsuarioController } from '../controllers/Usuario/Visualizar/VisualizarUsuario.controller';
+
+// HealthCheck
 
 container.registerSingleton<HealthcheckController>(
     'HealthcheckController',
     HealthcheckController,
 );
 
+// Usuário
+
+container.registerSingleton<AutenticarUsuarioController>(
+    'AutenticarUsuarioController',
+    AutenticarUsuarioController,
+);
+
 container.registerSingleton<CadastrarUsuarioController>(
     'CadastrarUsuarioController',
     CadastrarUsuarioController,
+);
+
+container.registerSingleton<VisualizarUsuarioController>(
+    'VisualizarUsuarioController',
+    VisualizarUsuarioController,
 );
