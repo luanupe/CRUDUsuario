74ecda1e8493ee9d0375469a51aabd5df7b49432
Luan Oliveira <luan.oliveira@pebmed.com.br>
2022-05-04 22:05:37 -0300

    Adicionando funcionalidade de cadastro de Endereço

---
+++ app/controllers/Endereco/Cadastrar/CadastrarEndereco.controller.ts
@@ -0,0 +1,41 @@
+import { injectable, inject } from 'tsyringe';
+import { Request, Response, NextFunction } from 'express';
+import { AbstractController } from "../../../contracts/Abstract.controller";
+import { EnderecoEntity } from '../../../entities/Endereco.entity';
+import { CadastrarEnderecoUsecase } from '../../../usecases/Endereco/CadastrarEndereco.usecase';
+
+@injectable()
+export class CadastrarEnderecoController extends AbstractController {
+
+    constructor(
+        @inject('CadastrarEnderecoUsecase')
+        private cadastrarEnderecoUsecase: CadastrarEnderecoUsecase,
+    ) {
+        super();
+    }
+
+    handle = async (request: Request, response: Response, next: NextFunction) => {
+        try {
+            // Retrieve data
+            const id = request.usuario.id;
+            const logradouro = request.body.logradouro;
+            const numero = request.body.numero;
+            const bairro = request.body.bairro;
+            const cidade = request.body.cidade;
+            const estado = request.body.estado;
+            const pais = request.body.pais;
+            const cep = request.body.cep;
+
+            // Act
+            const data = { logradouro, numero, bairro, cidade, estado, pais, cep, usuarioId: id };
+            const result: EnderecoEntity = await this.cadastrarEnderecoUsecase.run(data);
+
+            // OK
+            response.status(200).json(result);
+        }
+        catch (error) {
+            next(error);
+        }
+    }
+
+}
---
+++ app/usecases/Endereco/CadastrarEndereco.usecase.ts
@@ -0,0 +1,26 @@
+import { injectable, inject } from "tsyringe";
+import { Endereco } from "../../models/Endereco.model";
+import { EnderecoEntity } from "../../entities/Endereco.entity";
+import { EnderecoRepository } from "../../repositories/Endereco.repository";
+import { mapEndereco } from "../../adapters/Endereco.adapter";
+
+@injectable()
+export class CadastrarEnderecoUsecase {
+
+    constructor(
+        @inject('EnderecoRepository')
+        private enderecoRepository: EnderecoRepository,
+    ) {}
+
+    run = async (data: Partial<Endereco>): Promise<EnderecoEntity> => {
+        // Criar endereço
+        const id = await this.enderecoRepository.insert(data);
+
+        // Buscar endereço
+        const endereco = await this.enderecoRepository.getById(id);
+
+        // Result
+        return mapEndereco(endereco);
+    };
+
+}
